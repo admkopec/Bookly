e78668b076026a8154c7403a4d37bcdf6c23710e
Adam Kopeć <adamkopec5@icloud.com>
2023-01-05 21:53:05 +0100

    Added skeletons for some views

--- App.js
+++ App.js
@@ -8,147 +8,24 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import SearchView from "./Views/SearchView";
+import BookingsView from "./Views/BookingsView";
+import AccountView from "./Views/AccountView";
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
-    return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={backgroundStyle}>
-            <Header />
-            <View
-                style={{
-                    backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                }}>
-                <Section title="Step One">
-                    Edit <Text style={styles.highlight}>App.js</Text> to change this
-                    screen and then come back to see your edits.
-                </Section>
-                <Section title="See Your Changes">
-                    <ReloadInstructions />
-                </Section>
-                <Section title="Debug">
-                    <DebugInstructions />
-                </Section>
-                <Section title="Learn More">
-                    Read the docs to discover what to do next:
-                </Section>
-                <LearnMoreLinks />
-            </View>
-        </ScrollView>
-        </SafeAreaView>
-    );
-}
-
-const Stack = createNativeStackNavigator();
-
-const HomeStack = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Search"
-                options={{
-                    headerLargeTitle: true,
-                }}
-                component={HomeScreen}
-            />
-        </Stack.Navigator>
-    )
-}
-
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-
       <NavigationContainer>
           <Tab.Navigator screenOptions={{ headerShown: false }}>
-              <Tab.Screen name="Search" options={{ headerLargeTitle: true }} component={HomeStack}/>
+              <Tab.Screen name="Search" options={{ headerLargeTitle: true }} component={SearchView}/>
+              <Tab.Screen name="Bookings" options={{ headerLargeTitle: true }} component={BookingsView}/>
+              <Tab.Screen name="Account" options={{ headerLargeTitle: true }} component={AccountView}/>
           </Tab.Navigator>
       </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
